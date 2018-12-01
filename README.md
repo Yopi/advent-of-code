01e0d9b93554be3fe30cc6221e893ca41083685c
Jesper Bränn <jesper.brann@tink.se>
2018-12-01 13:57:18 +0100

    Initial commit, base structure

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2018
+
+Clojure solutions to Advent of Code 2018
+
+## Usage
+
+FIXME
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject adventofcode "0.1"
+  :description "Advent of code 2018"
+  :url "http://example.com/FIXME"
+  :license {:name "The Unlicense"
+            :url "https://unlicense.org"}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:resource-paths ["resources"]}})
