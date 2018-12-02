7fbe12292e9d8ab51512db976acd05d902d12456
Jesper Bränn <jesper.brann@tink.se>
2018-12-02 10:25:08 +0100

    Day 02: Add solutions for p1, p2
    Not super nice code, and a little bit hard to follow.

---
+++ src/adventofcode/day02.clj
@@ -0,0 +1,38 @@
+(ns adventofcode.day02
+    (:require
+        [clojure.java.io :as io]
+        [clojure.string :as str]
+    ))
+
+(def inputdata
+    (str/split
+        (slurp (io/file (io/resource  "day02/input.txt")))
+        #"\n"))
+
+(def testdata
+    (str/split
+        (slurp (io/file (io/resource  "day02/example.txt")))
+        #"\n"))
+
+(def testdata2
+    (str/split
+        (slurp (io/file (io/resource  "day02/example2.txt")))
+        #"\n"))
+
+(def part1
+    (*
+        (count (remove nil? (map #(some #{3} %) get-occurences)))
+        (count (remove nil? (map #(some #{2} %) get-occurences)))))
+
+(defn get-occurences
+    (map set (map vals (map frequencies (map seq inputdata)))))
+
+(def part2
+    (apply str (first
+        (filter #(= (count %) 25) ; Get off-by-one
+            (map  #(remove nil? %) ; So we can remove elements we failed to match
+                (mapcat identity ; Flatten one layer
+                    (for [x inputdata]
+                        (filter #(vector? %) ; Matching with itself
+                            (remove nil? ; No matches
+                                (map #(get % 2) (map #(data/diff (seq x) %) (map seq inputdata))))))))))))
