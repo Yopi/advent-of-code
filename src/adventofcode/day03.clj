ea5a3a6430a23634c5f334a12ac85076aad25629
Jesper Bränn <jesper.brann@tink.se>
2018-12-03 20:54:17 +0100

    Day 03: Make code actually runnable

--- src/adventofcode/day03.clj
+++ src/adventofcode/day03.clj
@@ -3,8 +3,13 @@
         [clojure.java.io :as io]
         [clojure.string :as str]
         [clojure.pprint :as pp]
-        [clojure.core.matrix :as matrix]
-    ))
+        [clojure.core.matrix :as m])
+    (:use clojure.core.matrix.operators))
+
+(defn set-current-implementation [impl]
+    (m/set-current-implementation impl))
+
+(set-current-implementation :vectorz)
 
 (def inputdata
     (map #(str/split % #": ")
@@ -32,10 +37,10 @@
 (defn offset-size-to-matrix [[offset size]]
     (let [s-offset (str/split offset #",")
           s-size (str/split size #",")
-          mx (matrix (to-array-2d (repeat matrix-height (repeat matrix-width 0))))]
+          mx (m/matrix (to-array-2d (repeat matrix-height (repeat matrix-width 0))))]
      (doseq [r (range (Integer. (first s-offset)) (+ (Integer. (first s-offset)) (Integer. (first s-size))))
            w (range (Integer. (second s-offset)) (+ (Integer. (second s-offset)) (Integer. (second s-size))))]
-           (set-selection! mx r w 1))
+           (m/set-selection! mx r w 1))
     mx))
 
 (def input-to-matrix
@@ -45,13 +50,13 @@
     (reduce + (map offset-size-to-matrix testdata)))
 
 ;; Part 1
-(defn input-to-number [mx]
-    (count (filter #(> % 1) (eseq mx))))
+(defn get-overlapping-inches [mx]
+    (count (filter #(> % 1) (m/eseq mx))))
 
 ;; Part 2
 (defn find-non-overlapping-id [i]
     ; For every elf, multiply with elf matrix and see if there is anything but ones
-    (if (= (count (filter #(> % 1) (eseq (* mx (offset-size-to-matrix (nth inputdata i)))))) 0)
+    (if (= (count (filter #(> % 1) (m/eseq (* input-to-matrix (offset-size-to-matrix (nth inputdata i)))))) 0)
         ; If not, then we have found the correct
         (+ i 1)
         ; Otherwise try the next elf
