ee8820de58f6e9a455e93fbb7cd93aea28a21935
Jesper Bränn <brann.jesper@gmail.com>
2019-12-01 21:34:12 +0100

    Day 1: Add solution for both parts

---
+++ src/adventofcode/2019/day01.clj
@@ -0,0 +1,30 @@
+(ns adventofcode.2019.day01
+    (:require
+        [clojure.java.io :as io]
+        [clojure.string :as str]
+        [clojure.set :as set]))
+
+(def inputdata
+    (map #(Integer. %)
+        (str/split
+            (slurp (clojure.java.io/file (clojure.java.io/resource  "2019/day01/input.txt")))
+            #"\n")))
+
+(defn calculate-mass [in]
+    (max ; Handle negative fuel
+        (- (quot in 3) 2)
+        0))
+
+(defn calculate-fuel [in]
+    (loop [cnt in
+           acc 0]
+           (if (zero? cnt)
+            acc
+            (recur (calculate-mass cnt) (+ acc (calculate-mass cnt))))
+    ))
+
+(def part1
+    (reduce + (map calculate-mass inputdata)))
+
+(def part2
+    (reduce + (map calculate-fuel inputdata)))
