8f5ee722629bb9a225abb8481ef459806070cc1c
Jesper Bränn <brann.jesper@gmail.com>
2019-12-07 10:21:26 +0100

    Day 7: Part 2
    Parallelism and same solution for part 1 and 2

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "The Unlicense"
             :url "https://unlicense.org"}
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.6.532"]
                  [net.mikera/core.matrix "0.62.0"]
                  [net.mikera/vectorz-clj "0.48.0"]
                  [ubergraph "0.8.1"]]
