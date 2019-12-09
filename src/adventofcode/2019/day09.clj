34802b6f7956d22ca1833112000d3dc298ca35f0
Jesper Bränn <brann.jesper@gmail.com>
2019-12-09 19:19:28 +0100

    Day 9: Make intcode computer work
    Parameter position when writing 😢

--- src/adventofcode/2019/day09.clj
+++ src/adventofcode/2019/day09.clj
@@ -18,7 +18,7 @@
     (zipmap (range (count in)) in))
 
 (defn value-at-position [data, pos]
-    (get data (get data pos ) ))
+    (get data (get data pos 0) 0))
 
 (defn parse-opcode [param])
 
@@ -26,7 +26,7 @@
     (case param-mode
         0   (value-at-position data position)
         1   (get data position 0)
-        ;2   (get data (+ (get data position 0) relative-base) 0)
+        2   (get data (+ (get data position 0) relative-base) 0)
     ))
 
 
@@ -49,82 +49,91 @@
                         p2 (mod (int (/ (get data index) 1000)) 10)
                         p3 (mod (int (/ (get data index) 10000)) 10)]
                     (do
-                        (println opc)
-                        (println (get-value data p1 (+ index 1) relative-base) (get-value data p2 (+ index 2) relative-base) (get-value data p3 (+ index 3) relative-base))
-                        (println data)
+                        ;(println opc)
+                        ;(println (get-value data p1 (+ index 1) relative-base) (get-value data p2 (+ index 2) relative-base) (get-value data p3 (+ index 3) relative-base))
                     (case opcode
                         1   (recur
                                 ; Data
-                                (assoc data (get-value data 1 (+ index 3) relative-base)
-                                    (+ (get-value data p1 (+ index 1) relative-base) (get-value data p2 (+ index 2)  relative-base)))
+                                (assoc data (alternative-get-value data p3 (+ index 3) relative-base)
+                                    (+ (get-value data p1 (+ index 1) relative-base) (get-value data p2 (+ index 2) relative-base)))
                                 ; Index
                                 (+ index 4)
+                                ; RB
                                 relative-base)
                         2   (recur
                                 ; Data
-                                (assoc data (get-value data 1 (+ index 3) relative-base)
+                                (assoc data (alternative-get-value data p3 (+ index 3) relative-base)
                                     (* (get-value data p1 (+ index 1) relative-base) (get-value data p2 (+ index 2) relative-base)))
                                 ; Index
                                 (+ index 4)
+                                ; RB
                                 relative-base)
 
                         3   (recur
-                                ; Get input
-                                (let [in (async/<! input)]
-                                    ; Data
-                                    (assoc data (alternative-get-value data p1 (+ index 1) relative-base) in)
-                                )
+                                ; Data
+                                (assoc data (alternative-get-value data p1 (+ index 1) relative-base) (async/<! input))
                                 ; Index
                                 (+ index 2)
+                                ; RB
                                 relative-base)
                         4   (do
-                                (println  "Output " (get-value data p1 (+ index 1) relative-base))
+                                (println "Output " (get-value data p1 (+ index 1) relative-base))
                                 (reset! last-output (get-value data p1 (+ index 1) relative-base))    ; Set last-output (For final state)
                                 (async/>! output (get-value data p1 (+ index 1) relative-base))       ; Write output to output channel
                                 (recur
+                                    ; Data
                                     data
+                                    ; Index
                                     (+ index 2)
+                                    ; RB
                                     relative-base))
                         5   (recur
                                 ; Data
                                 data
                                 ; Index
-                                ; If p1 != 0, goto p2, otherwise go forward
+                                ; If p1 != 0, then jump to p2. Otherwise go forward
                                 (if (not= (get-value data p1 (+ index 1) relative-base) 0)
                                     (get-value data p2 (+ index 2) relative-base)
                                     (+ index 3))
-                                ; Relative base
+                                ; RB
                                 relative-base)
                         6   (recur
                                 ; Data
                                 data
                                 ; Index
-                                ; If p1 == 0, goto p2, otherwise go forward
+                                ; If p1 == 0, then go to p2, otherwise continue
                                 (if (= (get-value data p1 (+ index 1) relative-base) 0)
                                     (get-value data p2 (+ index 2) relative-base)
                                     (+ index 3))
-                                ; Relative base
+                                ; RB
                                 relative-base)
                         7   (recur
                                 ; If p1 < p2; Set p3 = 1, otherwise p3 = 0
                                 (if (< (get-value data p1 (+ index 1) relative-base) (get-value data p2 (+ index 2) relative-base))
-                                    (assoc data (get-value data p3 (+ index 3) 0) 1)
-                                    (assoc data (get-value data p3 (+ index 3) 0) 0))
+                                    (assoc data (alternative-get-value data p3 (+ index 3) relative-base) 1)
+                                    (assoc data (alternative-get-value data p3 (+ index 3) relative-base) 0))
                                 ; Index
                                 (+ index 4)
+                                ; RB
                                 relative-base)
                         8   (recur
                                 ; If p1 == p2; Set p3 = 1, otherwise p3 = 0
                                 (if (= (get-value data p1 (+ index 1) relative-base) (get-value data p2 (+ index 2) relative-base))
-                                    (assoc data (get-value data p3 (+ index 3) 0) 1)
-                                    (assoc data (get-value data p3 (+ index 3) 0) 0))
+                                    (assoc data (alternative-get-value data p3 (+ index 3) relative-base) 1)
+                                    (assoc data (alternative-get-value data p3 (+ index 3) relative-base) 0))
                                 ; Index
                                 (+ index 4)
+                                ; RB
                                 relative-base)
-                        9   (recur data (+ index 2) (+ relative-base (get-value data p1 (+ index 1) relative-base)))
-                        99  @last-output)
-                    )
-                )))))
+                        9   (recur
+                                ; Data
+                                data
+                                ; Index
+                                (+ index 2)
+                                ; RB
+                                (+ relative-base (get-value data p1 (+ index 1) relative-base)))
+                        99   @last-output)))
+                ))))
 
 
 (defn initialize-channels [input-seq, channels]
