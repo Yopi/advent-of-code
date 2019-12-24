1f7a9ad2b33e2068e4c78c630abb9eed51d6920f
Jesper Bränn <brann.jesper@gmail.com>
2019-12-24 12:37:23 +0100

    Christmas Eve: Part 2 🤶

--- src/adventofcode/2019/day24.clj
+++ src/adventofcode/2019/day24.clj
@@ -5,7 +5,7 @@
         [clojure.string :as str]
         [clojure.set :as set]))
 
-(defn read-input [in]
+(defn read-input-p1 [in]
     (let [m
         (as-> (clojure.java.io/resource in) i
             (slurp i)
@@ -18,8 +18,26 @@
                   x (range w)]
                 {[(- x 2) (- y 2)] (nth (nth m y) x)})))))
 
-(def inputdata (read-input "2019/day24/input.txt"))
-(def testdata  (read-input "2019/day24/test.txt"))
+(defn read-input-p2 [in]
+    (let [m
+        (as-> (clojure.java.io/resource in) i
+            (slurp i)
+            (str/split i #"\n")
+            (map #(str/split % #"") i)
+            )]
+        (let [h (count m)
+              w (count (first m))]
+            (into {} (for [y (range h)
+                  x (range w)]
+                {[(- x 2) (- y 2) 0] (nth (nth m y) x)})))))
+
+
+(def inputdata (read-input-p1 "2019/day24/input.txt"))
+(def testdata  (read-input-p1 "2019/day24/test.txt"))
+
+(def inputdata-p2 (read-input-p2 "2019/day24/input.txt"))
+(def testdata-p2  (read-input-p2 "2019/day24/test.txt"))
+
 
 (defn borders [map]
     (let [positions (keys map)
@@ -102,4 +120,118 @@
 
 (defn part1 [m]
     (biodiversity (get (tick m 100) :map)))
+
+;; P2
+
+(defn bugs-adjacent-p2 [m x y level]
+    (count (filter #(= % "#")
+    ;(filter #(or (= % "#") (= % "."))
+    (concat
+        (if (and (= x 1) (= y 0))
+            [(get m [2 -2 (+ level 1)] ".")
+             (get m [2 -1 (+ level 1)] ".")
+             (get m [2 0 (+ level 1)] ".")
+             (get m [2 1 (+ level 1)] ".")
+             (get m [2 2 (+ level 1)] ".")]
+            [])
+        (if (and (= x -1) (= y 0))
+            [(get m [-2 -2 (+ level 1)] ".")
+             (get m [-2 -1 (+ level 1)] ".")
+             (get m [-2 0 (+ level 1)] ".")
+             (get m [-2 1 (+ level 1)] ".")
+             (get m [-2 2 (+ level 1)] ".")]
+            [])
+        (if (and (= x 0) (= y -1))
+            [(get m [-2 -2 (+ level 1)] ".")
+             (get m [-1 -2 (+ level 1)] ".")
+             (get m [0 -2 (+ level 1)] ".")
+             (get m [1 -2 (+ level 1)] ".")
+             (get m [2 -2 (+ level 1)] ".")]
+            [])
+        (if (and (= x 0) (= y 1))
+            [(get m [-2 2 (+ level 1)] ".")
+             (get m [-1 2 (+ level 1)] ".")
+             (get m [0 2 (+ level 1)] ".")
+             (get m [1 2 (+ level 1)] ".")
+             (get m [2 2 (+ level 1)] ".")]
+            [])
+        (if (= x 2)
+            [(get m [1 0 (- level 1)] ".")]
+            [])
+        (if (= x -2)
+            [(get m [-1 0 (- level 1)] ".")]
+            [])
+        (if (= y -2)
+            [(get m [0 -1 (- level 1)] ".")]
+            [])
+        (if (= y 2)
+            [(get m [0 1 (- level 1)] ".")]
+            [])
+        [
+            (if (or (= [(- x 1) y] [0 0]) (= x -2)) "?" (get m [(- x 1) y level] "."))
+            (if (or (= [(+ x 1) y] [0 0]) (= x 2)) "?" (get m [(+ x 1) y level] "."))
+            (if (or (= [x (- y 1)] [0 0]) (= y -2)) "?" (get m [x (- y 1) level] "."))
+            (if (or (= [x (+ y 1)] [0 0]) (= y 2)) "?" (get m [x (+ y 1) level] "."))
+        ]))
+        ;)
+        ))
+
+(defn calc-tick-p2 [m]
+    (let [start-x (apply min (map first (keys m)))
+          start-y (apply min (map second (keys m)))
+          start-level (- (apply min (map #(nth % 2) (keys m))) 1)
+          h (+ (apply max (map second (keys m))) 1)
+          w (+ (apply max (map first (keys m))) 1)
+          max-level (+ (apply max (map #(nth % 2) (keys m))) 1)]
+        (into {}
+            (for [level (range (- start-level 1) (+ max-level 1))
+                  y (range start-x h)
+                  x (range start-y w)]
+                (let [adjacent (bugs-adjacent-p2 m x y level)
+                      is-bug? (= (get m [x y level]) "#")]
+                    (if (= [x y] [0 0])
+                        {[x y level] "?"}
+                        (if is-bug?
+                            (if (= adjacent 1)
+                                {[x y level] "#"}
+                                {[x y level] "."})
+                            (if (or (= adjacent 1) (= adjacent 2))
+                                {[x y level] "#"}
+                                {[x y level] "."})
+                    ))
+               ))
+        )))
+
+(defn tick-p2 [m max-ticks]
+    (loop [m m
+           previous-ms []
+           ticks 0]
+        (let [flat-m (map second (sort-by first m))]
+            (if (some #(= flat-m %) previous-ms)
+                {:map m :ticks ticks :finished true}
+                (if (= ticks max-ticks)
+                    {:map m :ticks ticks :finished false}
+                    (recur (calc-tick-p2 m) (conj previous-ms flat-m) (+ ticks 1))
+                )))))
+
+(defn paint-p2 [m]
+    (let [corners [[-2 2] [-2 2]]
+            x-vals (first corners)
+            y-vals (second corners)
+            start-level (apply min (map #(nth % 2) (keys m)))
+            end-level (apply max (map #(nth % 2) (keys m)))
+            ]
+        (for [level (range start-level end-level)]
+            (do
+            (println "Depth: " level)
+            (vec (for [y (range (first y-vals) (+ (second y-vals) 1))]
+                (println (vec (flatten (for [x (range (first x-vals) (+ (second x-vals) 1))]
+                    (let [tile (get m [x y level] )]
+                        [tile]
+                    ))))
+                )))))))
+
+(defn part2 [m]
+    (count (filter #(= % "#") (vals (get (tick-p2 m 200) :map)))))
+
 
