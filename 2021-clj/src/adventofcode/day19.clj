3be9cb41715bf181cf31a9733cc9d380e9041bb5
Jesper Bränn <brann.jesper@gmail.com>
2021-12-20 06:13:01 +0100

    Day 19: Part 2

--- 2021-clj/src/adventofcode/day19.clj
+++ 2021-clj/src/adventofcode/day19.clj
@@ -108,7 +108,13 @@
                                        [-1 0 0]] [x y z]))
    (map (fn [x] (int x)) (matrix/mmul [[0 1 0]
                                        [0 0 -1]
-                                       [-1 0 0]] [x y z]))])
+                                       [-1 0 0]] [x y z]))
+   (map (fn [x] (int x)) (matrix/mmul [[-1 0 0]
+                                       [0 -1 0]
+                                       [0 0 -1]] [x y z]))
+   (map (fn [x] (int x)) (matrix/mmul [[0 0 1]
+                                       [0 1 0]
+                                       [1 0 0]] [x y z]))])
 
 (defn all-possible-rotations [in]
   (let [possible-rotations (map possible-rotations in)]
@@ -130,23 +136,41 @@
         (for [[x2 y2 z2] b]
           {[(- x2 x1) (- y2 y1) (- z2 z1)] 1})))))
 
-(defn p1 [in]
+(defn solve [in]
   (loop [comparison     (into #{} (in "0"))
          comparing-key  (first (rest (keys in)))
-         to-be-compared (shuffle (rest (rest (keys in))))]
+         to-be-compared (shuffle (rest (rest (keys in))))
+         scanners       [[0 0 0]]]
     (if (nil? comparing-key)
-      comparison
+      {:beacons  comparison
+       :scanners scanners}
       (let [comparing          (in comparing-key)
             possible-rotations (all-possible-rotations comparing)
             normalized         (filter some?
                                  (for [rotation possible-rotations]
-                                   (let [comparisons      (compare-coordinate-systems comparison rotation)
-                                         scanner-positions-filtered (into {} (filter (fn [[k v]] (> v 6)) comparisons))]
+                                   (let [comparisons                (compare-coordinate-systems comparison rotation)
+                                         scanner-positions-filtered (into {} (filter (fn [[k v]] (>= v 6)) comparisons))]
                                      (if (seq scanner-positions-filtered)
                                        (let [scanner-position (key (apply max-key val scanner-positions-filtered))]
-                                         (normalize-coords scanner-position rotation))
+                                         {:normalized (normalize-coords scanner-position rotation)
+                                          :scanner    scanner-position})
                                        nil))))]
         (println "Comparing " comparing-key "|" "To be compared" to-be-compared)
         (if (empty? normalized)
-          (recur comparison (first to-be-compared) (concat (rest to-be-compared) [comparing-key]))
-          (recur (set/union comparison (into #{} (first normalized))) (first to-be-compared) (rest to-be-compared)))))))
+          (recur comparison (first to-be-compared) (concat (rest to-be-compared) [comparing-key]) scanners)
+          (recur (set/union comparison (into #{} ((first normalized) :normalized))) (first to-be-compared) (rest to-be-compared) (concat scanners [((first normalized) :scanner)])))))))
+
+(defn p1 [in]
+  (let [{:keys [beacons scanners]} (solve in)]
+    (count beacons)))
+
+(defn p2 [in]
+  (let [{:keys [beacons scanners]} (solve in)]
+    (apply max
+      (for [[x1 y1 z1] scanners]
+        (apply max
+          (for [[x2 y2 z2] scanners]
+            (+
+             (Math/abs (- x1 x2))
+             (Math/abs (- y1 y2))
+             (Math/abs (- z1 z2)))))))))
